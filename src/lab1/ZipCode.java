f8f51b518a8408552e2d188e2a50265c0687b419
Vedika894 <vjain894@yahoo.com>
2025-08-30 18:40:26 -0400

    added the code in the ZipCode class

--- src/lab1/ZipCode.java
+++ src/lab1/ZipCode.java
@@ -9,5 +9,149 @@ package lab1;
  * @author Vedika
  */
 public class ZipCode {
+    private int zip; //int 
+    private boolean valid; 
+    private String errorMessage; 
+            
+    public ZipCode(int zip) {
+       if (zip <0 ) {
+           errorMessage = "ZIP cannot be negative";
+           System.out.println(errorMessage);
+           valid = false; 
+       }
+        if (zip < 99999 ) {
+            errorMessage = "Zip cannot be more than 5 digits ";
+            System.out.println(errorMessage);
+            valid = false; 
+        }
+        else {
+            this.zip = zip; 
+            valid = true; 
+        }
+    }
     
+    public ZipCode (String barCode) {
+        if (barCode == null) {
+            valid = false; 
+            errorMessage = "Bar code cannnot be null ";
+            return;   //dont continue 
+        }
+    }
+    
+    //getBarCode 
+    public String getBarCode() {
+        if (!valid) {
+            return errorMessage;
+        }
+        
+        //zero padding 
+        String zipString = String.format ("%05d",zip);
+        
+        String result = "1";
+         
+        
+        //encode each digit into a string of 0 and 1 
+        for (int i = 0; i < zipString.length(); i++) {
+             int digit = zipString.charAt(i) -'0';
+             result += encode(digit);
+        }
+        
+        result += '1';
+        
+        return result; 
+}                 
+    
+    //ParseBarCode gives us a integer as an answer 
+    private int ParseBarCode(String barCode) {
+        //check for errors 
+        String middle = barCode.substring(1, barCode.length()-1); //to recheck
+        //check if the middle part can be divided into groups of 5
+        if (middle.length() % 5 != 0 ) {
+            valid = false; 
+            return -1; 
+        }
+        
+        if (barCode.charAt(0) != '1' || barCode.charAt() != "1") {
+            return -1; 
+        }
+        
+        
+        //check if only only 0 and 1's
+        for (int i = 0 ; i < barCode.length(); i++) {
+            if (barCode.charAt(i) != 0 || barCode.charAt(i) != 1) {
+                valid = false; 
+                errorMessage = "barCode can only contain 0 and 1's ";
+                return -1; 
+            }
+        }
+            
+            //check if there are exactly two 1's 
+            //tranverse the string middle 
+            for ( int i = 0 ; i < 5; i++) {
+                String group = middle.substring(i*5, i*5 +5);
+                
+                
+                //check the numbers of ones 
+                int ones = 0; 
+                
+                for (int j = 0 ; j < 5; j++) {
+                    if (group.charAt(i) == '1') {
+                        ones++;
+                    }
+                    //error 
+                    if(ones != 2) {
+                        errorMessage = "there must be exacly two 1's";
+                        System.out.println("errorMessage");
+                        return -1; 
+                    }
+                }
+                
+                int digit = decodeGroup(group);
+                if (digit  == -1)
+                
+                barCode += digit; 
+            
+            
+        }
+        
+    }
+    
+    
+    //helper method 
+    //encode, integer to string 
+    private String encode (int digit) {
+        switch (digit) {
+            case 1 : return "11000";
+            case 2 : return "00011";
+            case 3 : return "00101";
+            case 4 : return "01010";
+            case 5 : return "01010";
+            case 6 : return "01100";
+            case 7 : return "10010";
+            case 8 : return "10010";
+            case 9 : return "10100";
+            default : return "";         
+        }
+    }
+    
+    //helper method 
+    //decode // a group of bit code back to integer 
+    public int decodeGroup (String group) {
+        int sum = 0; 
+        if (group.charAt(0) == '1') sum += 7; 
+        if (group.charAt(1) == '1') sum += 4;
+        if (group.charAt(2) == '1') sum += 2;
+        if (group.charAt(3) == '1') sum += 1; 
+        if (group.charAt(4) == '1') sum += 0; 
+        
+        
+        if (sum == 11) {
+            return 0; 
+        } else if (sum > 0 && sum <= 9) {
+            return sum; 
+        } else {
+            return -1; //invalid 
+        }
+                
+    }
 }
